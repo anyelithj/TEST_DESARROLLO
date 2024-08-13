99adf5f1f59faa49c7f352b569a8060a6aa730f4
anyelithj <anyelithj@misena.edu.co>
2024-08-13 07:09:16 -0500

    refactor in css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Tabs from './component/Tabs/Tabs';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Tabs from "./component/Tabs/Tabs";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-        <Route path="/" element={<Tabs />} />
+          <Route path="/" element={<Tabs />} />
           <Route path="/colombia_dash" element={<Tabs />} />
         </Routes>
       </Router>
